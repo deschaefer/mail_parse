2d7415a99b3001e01828fd28aa2233b9d23017c5
Don Schaefer <deschaefer@gmail.com>
2020-10-22 22:12:01 -0400

    read and write from s3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 var htmlparser = require("htmlparser2");
+const { BlobServiceClient } = require('@azure/storage-blob');
+const { v1: uuid} = require('uuid');
 
 function traverse(an_array) {
 
@@ -56,7 +58,66 @@ function parse_mail_for_partners(the_mail_string) {
     return traverse(the_dom)
 }
 
-var file_data = "";
-file_data = fs.readFileSync('sample.html', "utf-8");
+// A helper function used to read a Node.js readable stream into a string
+async function streamToString(readableStream) {
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+        readableStream.on("data", (data) => {
+            chunks.push(data.toString());
+        });
+        readableStream.on("end", () => {
+            resolve(chunks.join(""));
+        });
+        readableStream.on("error", reject);
+    });
+}
 
-console.log(parse_mail_for_partners(file_data));
+const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
+
+async function main() {
+
+    // Create the BlobServiceClient object which will be used to create a container client
+    const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+
+    // Create a unique name for the container
+    const containerName = 'quickstart' + uuid();
+
+    console.log('\nCreating container...');
+    console.log('\t', containerName);
+
+    // Get a reference to a container
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    // Create the container
+    const createContainerResponse = await containerClient.create();
+    console.log("Container was created successfully. requestId: ", createContainerResponse.requestId);
+
+
+    // Create a unique name for the blob
+    const blobName = 'quickstart' + uuid() + '.txt';
+
+    // Get a block blob client
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    console.log('\nUploading to Azure storage as blob:\n\t', blobName);
+
+    // Upload data to the blob
+    var file_data = "";
+    file_data = fs.readFileSync('sample.html', "utf-8");
+    const uploadBlobResponse = await blockBlobClient.upload(file_data, file_data.length);
+    console.log("Blob was uploaded successfully. requestId: ", uploadBlobResponse.requestId);
+
+    // Get blob content from position 0 to the end
+    // In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
+    // In browsers, get downloaded data by accessing downloadBlockBlobResponse.blobBody
+    const downloadBlockBlobResponse = await blockBlobClient.download(0);
+    console.log('\nDownloaded blob content...');
+
+    var data_body = await streamToString(downloadBlockBlobResponse.readableStreamBody)
+
+    console.log('\t', data_body);
+
+    console.log(parse_mail_for_partners(data_body));
+}
+
+main().then(() => console.log('Done')).catch((ex) => console.log(ex.message));
